1603ce506fc9c0ec703c39206ffda7f769cb592b
OzlemTS <ozlemturkerii@gmail.com>
2020-01-03 15:19:25 +0300

    win rules added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,57 @@
 let symbols = ['X','O'];
-let move = [0,1];
-let areas = [0,1,2,3,4,5,6,7,8];
+let move = 0;
+let areas = ['0','1','2','3','4','5','6','7','8'];
+let board = ['','','','','','','','','']
 
-const container = document.querySelector(".container");
+const containerElement = document.querySelector(".container");
+const boxElements = document.querySelectorAll(".box")
 
 
-
-container.addEventListener('click', e => {
+containerElement.addEventListener('click', e => {
     if (e.target.className == 'box') { 
-    e.target.innerHTML = 'X';
-    const chosenindex = e.target.getAttribute('data-key');
-    areas.splice(chosenindex, 1);
-    e.target.classList.add('disabledEvents');
-    //delete areas[e.target.getAttribute('data-key')];
-    console.log(areas);
+        e.target.innerText = 'X';
+        let datakey = e.target.getAttribute('data-key')
+        board[datakey] = 'X';
+        let chosen = areas.indexOf(datakey);
+        areas.splice(chosen, 1);
+        e.target.classList.add('disabledEvents');
+        console.log(datakey, typeof(datakey))
+        isWin('X');
+       
+ 
+        let computerChoiceIndex = Math.floor(Math.random() * (areas.length));
+        if (areas.length > 0) {
+        datakey = areas[computerChoiceIndex];
+        boxElements[datakey].innerText = 'O';
+        board[datakey] = 'O';
+        boxElements[datakey].classList.add('disabledEvents');
+        areas.splice(computerChoiceIndex, 1);
+        isWin('O');
+        }
+
     }
+console.log(board);
+
+
 })
 
 
+function isWin(symbol) {
+    if ((board[0] == symbol && board[1] == symbol && board[2] == symbol) || 
+        (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
+        (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
+        (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
+        (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
+        (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
+        (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
+        (board[2] == symbol && board[4] == symbol && board[6] == symbol) ) {
+            document.querySelector('.heading-2').innerText = symbol + 'WON!';
+            containerElement.classList.add('disabledEvents');
+        }
+    else {}
+}
 
-//disable the click event for computer move
-//container.classList.add('disabledEvents');
-console.log(Math.floor(Math.random() * 9));
+
+
 
 
