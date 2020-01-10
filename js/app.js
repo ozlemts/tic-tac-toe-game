df3bd0cddc7f64a9a890bc3f9f378e756fdb79cb
OzlemTS <ozlemturkerii@gmail.com>
2020-01-10 16:26:51 +0300

    formatting and result state

--- js/app.js
+++ js/app.js
@@ -1,33 +1,38 @@
+
+//VARIABLES
 let symbols = ['X','O'];
 let move = 0;
-let areas = ['0','1','2','3','4','5','6','7','8'];
+let avaible_numbers = ['0','1','2','3','4','5','6','7','8'];
 let board = ['','','','','','','','','']
 
-const containerElement = document.querySelector(".container");
-const boxElements = document.querySelectorAll(".box")
+const CONTAINER_ELEMENT = document.querySelector(".container");
+const BOX_ELEMENTS = document.querySelectorAll(".box")
+
+// 
+document.querySelector(".result").style.visibility = 'hidden';
 
 
-containerElement.addEventListener('click', e => {
+CONTAINER_ELEMENT.addEventListener('click', e => {
     if (e.target.className == 'box') { 
         e.target.innerText = 'X';
-        let datakey = e.target.getAttribute('data-key')
-        board[datakey] = 'X';
-        let chosen = areas.indexOf(datakey);
-        areas.splice(chosen, 1);
+        let data_key = e.target.getAttribute('data-key')
+        board[data_key] = 'X';
+        let chosen = avaible_numbers.indexOf(data_key);
+        avaible_numbers.splice(chosen, 1);
         e.target.classList.add('disabledEvents');
-        console.log(datakey, typeof(datakey))
+        console.log(data_key, typeof(data_key))
         isWin('X');
        
  
-        let computerChoiceIndex = Math.floor(Math.random() * (areas.length));
-        if (areas.length > 0) {
-        datakey = areas[computerChoiceIndex];
-        boxElements[datakey].innerText = 'O';
-        board[datakey] = 'O';
-        boxElements[datakey].classList.add('disabledEvents');
-        areas.splice(computerChoiceIndex, 1);
-        isWin('O');
-        }
+        let computer_choice_index = Math.floor(Math.random() * (avaible_numbers.length));
+        if (avaible_numbers.length > 0) {
+            data_key = avaible_numbers[computer_choice_index];
+            BOX_ELEMENTS[data_key].innerText = 'O';
+            board[data_key] = 'O';
+            BOX_ELEMENTS[data_key].classList.add('disabledEvents');
+            avaible_numbers.splice(computer_choice_index, 1);
+            isWin('O');
+            }
 
     }
 console.log(board);
@@ -44,9 +49,11 @@ function isWin(symbol) {
         (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
         (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
         (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
-        (board[2] == symbol && board[4] == symbol && board[6] == symbol) ) {
-            document.querySelector('.heading-2').innerText = symbol + 'WON!';
-            containerElement.classList.add('disabledEvents');
+        (board[2] == symbol && board[4] == symbol && board[6] == symbol) ) 
+        {
+            document.querySelector('.heading-2').innerText = symbol + ' WON!';
+            BOX_ELEMENTS.forEach(e => e.classList.add('disabledEvents'));
+            document.querySelector(".result").style.visibility = 'visible';
         }
     else {}
 }
